984d37e53cd599b565d98ef339a16765eff28a14
Arjun7161 <akarjun7161@gmail.com>
2022-07-11 12:37:32 +0100

    uplo

---
+++ upload.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+require 'includes/db.php';
+
+if(!isset($_SESSION['studentid'])){
+    header('location:login.php');
+ }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Upload</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form action="uploadval.php" method="POST" enctype="multipart/form-data">
+    <label for="name">Student ID :</label>
+        <input type="text" name="names" id="names" required value="<?php echo $_SESSION['studentid']; ?>"><br><br>
+        <input type="file" name="file">
+        <button type="submit" name="submit">Upload Files</button>
+    </form>
+</body>
+</html>
---
+++ uploadval.php
@@ -0,0 +1,39 @@
+<?php
+$conn= mysqli_connect('localhost','root','','user');
+if (isset($_POST['submit'])) {
+    $file=$_FILES['file'];
+    $student=$_POST['names'];
+
+    $fileName=$_FILES['file']['name'];
+    $fileTmpName=$_FILES['file']['tmp_name'];
+    $fileSize=$_FILES['file']['size'];
+    $fileError=$_FILES['file']['error'];
+    $fileType=$_FILES['file']['type'];
+
+    $fileExt= explode('.', $fileName);
+    $fileActualExt= strtolower(end($fileExt));
+
+    $allowed= array('pdf','docx');
+
+    if (in_array($fileActualExt,$allowed)){
+        if($fileError === 0){
+            if ($fileSize < 5000000){
+                $fileNameNew= uniqid('').".".$fileActualExt;
+                $fileDestination='uploads/'.$fileNameNew;
+                move_uploaded_file($fileTmpName,$fileDestination);
+                $sql="insert into upload values ('$student','$fileName')";
+                mysqli_query($conn,$sql);
+                header("location:upload.php");
+
+            }else{
+                echo "File is too big";
+            }
+
+        }else{
+            echo "Error while uploading";
+        }
+
+    }else{
+        echo "File type Not allowed!";
+    }
+}
