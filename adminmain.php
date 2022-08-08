e246536a89c9b64eb5adb89025ac6ab1f729ad4e
Arjun7161 <akarjun7161@gmail.com>
2022-08-08 15:16:14 +0100

    hover

--- adminmain.php
+++ adminmain.php
@@ -14,13 +14,14 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Main</title>
     <link rel="stylesheet" href="style.css">
-   
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
 </head>
 <body>
-    <div class="hero">
-        <nav>
-            <h2 class="logo">i<span>Review</span></h2>
-            <ul>
+<div class="full-page">
+        <div class="navbar">
+            <h2>IReview</h2>
+            <nav>
+                <ul id='MenuItems'>
                 <h1>Welcome <?php echo $_SESSION['adminid']; ?></h1>
                 <li><a href="adminmain.php">Home</a></li>
                 <li><a href="create.php">Create Table</a></li>
@@ -31,6 +32,8 @@ session_start();
             </ul>
         </nav>
     </div>
-
+</div>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
 </body>
 </html>
